0f5c45d481ffd8f1926fe06f898c84189a02a8cb
Simon Cozens <simon@simon-cozens.org>
2024-04-01 20:24:26 +0100

    Use YAML, not strictYAML; black

--- Lib/notobuilder/__main__.py
+++ Lib/notobuilder/__main__.py
@@ -1,9 +1,9 @@
 import subprocess
 from os import chdir
 from pathlib import Path
+import yaml
 import sys
 
-from strictyaml import load
 from gftools.builder import GFBuilder, BASE_SCHEMA
 
 
@@ -12,18 +12,23 @@ def main(args=None):
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--graph", help="Draw a graph of the build process", action="store_true")
+    parser.add_argument(
+        "--graph", help="Draw a graph of the build process", action="store_true"
+    )
     parser.add_argument("--no-ninja", help="Do not run ninja", action="store_true")
-    parser.add_argument("--generate", help="Just generate and output recipe from recipe builder", action="store_true")
+    parser.add_argument(
+        "--generate",
+        help="Just generate and output recipe from recipe builder",
+        action="store_true",
+    )
     parser.add_argument("config", help="Path to config file")
     args = parser.parse_args(args)
 
     with open(args.config, "r") as file:
-        config = load(file.read(), BASE_SCHEMA)
+        config = yaml.safe_load(file.read())
     chdir(Path(args.config).resolve().parent)
 
     config["recipeProvider"] = "noto"
-
     pd = GFBuilder(config)
     if args.generate:
         print(yaml.dump(pd.config))
@@ -37,5 +42,6 @@ def main(args=None):
         result = subprocess.run(["ninja"])
         sys.exit(result.returncode)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
