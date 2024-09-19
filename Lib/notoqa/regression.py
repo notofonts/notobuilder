31065f3c817b32cb3d8a03cb01c70d041aa159d0
Simon Cozens <simon@simon-cozens.org>
2024-09-19 12:37:18 +0100

    Use diff3 for regression testing!

--- Lib/notoqa/regression.py
+++ Lib/notoqa/regression.py
@@ -2,8 +2,9 @@ import os
 import glob
 from gftools.actions.getlatestversion import get_latest_release
 from gftools.utils import download_files_from_archive
-from diffenator2 import ninja_diff, DFont
 from diffenator2.html import build_index_page
+import subprocess
+import re
 
 if not "GITHUB_TOKEN" in os.environ:
     raise ValueError("GITHUB_TOKEN was not passed to the notoqa environment")
@@ -35,7 +36,9 @@ for family in [os.path.basename(x) for x in glob.glob("fonts/*")]:
     os.makedirs(fonts_before_dir, exist_ok=True)
 
     fonts_before = download_files_from_archive(previous_url, fonts_before_dir)
-    variables_before = [f for f in fonts_before if f.endswith(".ttf") and "unhinted/variable-ttf" in f]
+    variables_before = [
+        f for f in fonts_before if f.endswith(".ttf") and "unhinted/variable-ttf" in f
+    ]
     fonts_now = glob.glob(f"fonts/{family}/unhinted/ttf/*.ttf")
     variables_now = glob.glob(f"fonts/{family}/unhinted/variable-ttf/*.ttf")
 
@@ -46,15 +49,13 @@ for family in [os.path.basename(x) for x in glob.glob("fonts/*")]:
         fonts_now = variables_now
         fonts_before = variables_before
 
-    dfonts_before = [DFont(f) for f in fonts_before]
-    dfonts_now = [DFont(f) for f in fonts_now]
     print("Fonts before: ")
     for s in fonts_before:
-        print(" * "+s)
+        print(" * " + s)
 
     print("Fonts now: ")
     for s in fonts_now:
-        print(" * "+s)
+        print(" * " + s)
 
     if not fonts_now:
         print(f"No current fonts to compare for {family}!")
@@ -62,8 +63,43 @@ for family in [os.path.basename(x) for x in glob.glob("fonts/*")]:
     if not fonts_before:
         print(f"No previous fonts to compare for {family}!")
         continue
-    ninja_diff(dfonts_before, dfonts_now, out=os.path.join(outdir, family),
-        user_wordlist=all_strings)
 
-if glob.glob(outdir+"/*"):
+    if len(fonts_before) == 1 and len(fonts_now) == 1:
+        subprocess.run(
+            [
+                "diffenator3",
+                fonts_before[0],
+                fonts_now[0],
+                "--html",
+                "--output",
+                os.path.join(outdir, family),
+                # user_wordlist=all_strings)
+            ],
+            check=True,
+        )
+    else:
+        # Try to match them up
+        for before in fonts_before:
+            before_bare = before.replace(os.path.join(outdir, "fonts_before"), "fonts")
+            if before_bare in fonts_now:
+                subprocess.run(
+                    [
+                        "diffenator3",
+                        before,
+                        before_bare,
+                        "--html",
+                        "--output",
+                        os.path.join(outdir, family),
+                        # user_wordlist=all_strings)
+                    ],
+                    check=True,
+                )
+                os.rename(
+                    os.path.join(outdir, family, "diffenator.html"),
+                    os.path.join(outdir, family, f"{os.path.basename(before)}.html"),
+                )
+            else:
+                print(f"Could not find a match for {before}")
+
+if glob.glob(outdir + "/*"):
     build_index_page(outdir)
